d53dfd14e21a6e073bae4143aac46229e80df3b0
wujinhjun <wujinhjun@outlook.com>
2023-11-28 15:58:18 +0800

    feat: the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,54 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+
 import HomePage from '@/pages/homePage';
+import ConfigPage from '@/pages/configPage';
+import ChatPage from '@/pages/chatPage';
+import ErrorPage from '@/pages/errorPage';
+import RootComponent from './pages/root';
 
 function App() {
   const router = createBrowserRouter([
     {
       path: '/',
+      //   Component() {
+      //     return (
+      //       <>
+      //         <HomePage />
+      //       </>
+      //     );
+      //   },
+      element: <HomePage />,
+      errorElement: <ErrorPage />
+    },
+    {
+      path: '/config',
       Component() {
         return (
           <>
-            <HomePage />
+            <ConfigPage />
+          </>
+        );
+      }
+    },
+    {
+      path: 'chat',
+      Component() {
+        return (
+          <>
+            <ChatPage />
           </>
         );
       }
     }
   ]);
-  return <RouterProvider router={router} />;
+
+  return (
+    <>
+      <Toaster />
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
