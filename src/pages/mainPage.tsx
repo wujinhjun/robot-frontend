4f7514948c75ba5af83711e64e69e2852ed327c1
wujinhjun <wujinhjun@outlook.com>
2023-12-14 18:40:47 +0800

    feat: main page

---
+++ src/pages/mainPage.tsx
@@ -0,0 +1,9 @@
+import style from '@/styles/mainPage.module.css';
+
+export default function MainPage() {
+  return (
+    <section className={`${style['container']}`}>
+      <div className={`${style['decorate-circle']}`} />
+    </section>
+  );
+}
