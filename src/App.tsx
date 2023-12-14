219d81e6c8dea801000f6db0286ffb4535a4d5dd
wujinhjun <wujinhjun@outlook.com>
2023-12-14 18:41:46 +0800

    feat: the root

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,45 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  NavLink,
+  useRoutes,
+  useLocation
+} from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+// import {  } from 'react-router-dom';
 
 import HomePage from '@/pages/homePage';
 import ConfigPage from '@/pages/configPage';
 import ChatPage from '@/pages/chatPage';
 import ErrorPage from '@/pages/errorPage';
 import RootComponent from './pages/root';
+import MainPage from './pages/mainPage';
+import StartPage from './pages/startPage';
+import ConsultPage from './pages/consultPage';
+import GamePage from './pages/gamePage';
+
+import style from './styles/mainpage.module.css';
 
 function App() {
-  const router = createBrowserRouter([
+  const routes = useRoutes([
+    { path: '/', element: <StartPage /> },
+    { path: '/config', element: <ConfigPage /> },
+    { path: '/chat', element: <ChatPage /> },
+    { path: '/consult', element: <ConsultPage /> },
     {
-      path: '/',
-      //   Component() {
-      //     return (
-      //       <>
-      //         <HomePage />
-      //       </>
-      //     );
-      //   },
-      element: <HomePage />,
-      errorElement: <ErrorPage />
-    },
-    {
-      path: '/config',
-      Component() {
-        return (
-          <>
-            <ConfigPage />
-          </>
-        );
-      }
-    },
-    {
-      path: 'chat',
-      Component() {
-        return (
-          <>
-            <ChatPage />
-          </>
-        );
-      }
+      path: '/game',
+      element: <GamePage />
     }
   ]);
 
   return (
     <>
+      <section className={`${style['container']}`}>
+        <div className={`${style['decorate-circle']}`} />
+        <section className="z-[2] h-full">{routes}</section>
+      </section>
       <Toaster />
-      <RouterProvider router={router} />
+      {/* <RouterProvider router={router} /> */}
     </>
   );
 }
