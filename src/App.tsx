adcf9693989c344f0abd84bf8d503573aaca87cb
wujinhjun <wujinhjun@outlook.com>
2023-12-25 23:32:49 +0800

    refactor: return icon

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ import MainPage from './pages/mainPage';
 import StartPage from './pages/startPage';
 import ConsultPage from './pages/consultPage';
 import GamePage from './pages/gamePage';
+import WakeUpPage from './pages/games/wakeUpPage';
 
 import style from './styles/mainpage.module.css';
 
@@ -29,17 +30,47 @@ function App() {
     {
       path: '/game',
       element: <GamePage />
+    },
+    {
+      path: '/game/wake-up',
+      element: <WakeUpPage />
     }
   ]);
 
   return (
     <>
-      <section className={`${style['container']}`}>
+      {/* <section className={`${style['container']}`}>
         <div className={`${style['decorate-circle']}`} />
-        <section className="z-[2] h-full">{routes}</section>
-      </section>
+        <section className="z-[2] h-full"></section>
+      </section> */}
       <Toaster />
       {/* <RouterProvider router={router} /> */}
+
+      <section className={`${style['content-container']}`}>
+        <div className={`${style['content-left-panel']}`}>
+          <div className={`${style['content-decorate-circle']}`}></div>
+        </div>
+        {routes}
+
+        <NavLink
+          to="/"
+          className={'w-fit row-start-1 row-end-2 col-start-1 col-end-2 z-[3]'}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="61"
+            height="89"
+            viewBox="0 0 61 89"
+            fill="none"
+            className="ml-16 mt-16"
+          >
+            <path
+              d="M44.3636 0L0 44.3636L44.3636 88.7273L61 72.0909L33.2727 44.3636L61 16.6364L44.3636 0Z"
+              fill="white"
+            />
+          </svg>
+        </NavLink>
+      </section>
     </>
   );
 }
